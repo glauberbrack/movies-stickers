edd681acb5e2c3b0e2de1a71fc16dcee4480ac15
glauberbrack <glauber@brack.com.br>
2023-03-27 23:42:18 -0300

    feat: parse json response

--- src/App.java
+++ src/App.java
@@ -3,10 +3,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // HTTP requests
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI uri_address = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -14,6 +17,16 @@ public class App {
         HttpResponse<String> response = client.send(requestTopMovies, BodyHandlers.ofString());
         String body = response.body();
         System.out.println(body);
-        
+
+        // Parse json to get only Title, Poster and Classification
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> moviesList = parser.parse(body);
+
+        for (Map<String, String> movie: moviesList) {
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+            System.out.println("");
+        }
     }
 }
