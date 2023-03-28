5978d9a0cbb5aadeb9648fd17eca7a3323e5d4c7
glauberbrack <glauber@brack.com.br>
2023-03-27 22:58:08 -0300

    feat: create request

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI uri_address = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var requestTopMovies = HttpRequest.newBuilder(uri_address).GET().build();
+        HttpResponse<String> response = client.send(requestTopMovies, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+        
     }
 }
